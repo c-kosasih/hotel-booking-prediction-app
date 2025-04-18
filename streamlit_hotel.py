803bfb5e3d542395b7981534ef9e13480d0594db
Clarissa Kosasih <142484694+c-kosasih@users.noreply.github.com>
2025-04-18 17:45:15 +0700

    Update streamlit_hotel.py

--- streamlit_hotel.py
+++ streamlit_hotel.py
@@ -69,7 +69,7 @@ def predict_booking_status(input_dict):
     required_columns = ['no_of_adults', 'no_of_children', 'no_of_weekend_nights', 'no_of_week_nights', 'type_of_meal_plan', 
                     'required_car_parking_space', 'room_type_reserved', 'lead_time', 'arrival_year', 'arrival_month', 
                     'arrival_date', 'market_segment_type', 'repeated_guest', 'no_of_previous_cancellations', 
-                    'no_of_previous_bookings_not_canceled', 'avg_price_per_room', 'no_of_special_requests']
+                    'no_of_previous_bookings_not_canceled', 'avg_price_per_room', 'no_of_special_requests', 'total_no_of_nights']
     
     df = df[required_columns]
     
